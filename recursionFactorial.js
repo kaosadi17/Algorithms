85f3102ed2eb78b8fc08cfdbcdf0e5875a77c6f8
Amali Osadi <kaosadi17@gmail.com>
2023-06-01 00:28:54 +0300

    Create recursionFactorial.js

---
+++ recursionFactorial.js
@@ -0,0 +1,10 @@
+function recursionFactorial(n){
+  if(n===0){
+    return 1;
+  }
+  return n * recursionFactorial(n-1);
+}
+
+console.log(recursionFactorial(0));
+console.log(recursionFactorial(1));
+console.log(recursionFactorial(5));
